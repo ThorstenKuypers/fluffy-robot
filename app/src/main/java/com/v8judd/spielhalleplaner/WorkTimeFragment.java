35a7af867ceef005e1ff59423ac3f1537dc31340
Thorsten Kuypers <v8judd@gmail.com>
2018-10-05 01:38:42 +0200

    started to implement DataBinding to WorkTimeFragment class

--- app/src/main/java/com/v8judd/spielhalleplaner/WorkTimeFragment.java
+++ app/src/main/java/com/v8judd/spielhalleplaner/WorkTimeFragment.java
@@ -3,7 +3,9 @@ package com.v8judd.spielhalleplaner;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,28 +13,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.v8judd.spielhalleplaner.databinding.WorkingTimesFragmentBinding;
+
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 public class WorkTimeFragment extends Fragment {
+
+    //private WorkingTimesViewModel mViewModel;
+
+    private WorkingTimesFragmentBinding _binding;
 
     public WorkTimeFragment() {
 
     }
+//    public static WorkTimeFragment newInstance() {
+//        return new WorkTimeFragment();
+//    }
 
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.working_times_fragment, container, false);
+        _binding = WorkingTimesFragmentBinding.inflate(inflater, container, false);
+        _binding.setVm(new String("Monday"));
 
-        rootView.findViewById(R.id.monday_start_time).setOnClickListener(new DisplayTimePickerDialog(getContext()));
+        //rootView.findViewById(R.id.monday_start_time).setOnClickListener(new DisplayTimePickerDialog(getContext()));
+        _binding.mondayStartTime.setOnClickListener(new DisplayTimePickerDialog(getContext()));
         rootView.findViewById(R.id.tuesday_start_time).setOnClickListener(new DisplayTimePickerDialog(getContext()));
         rootView.findViewById(R.id.wednesday_start_time).setOnClickListener(new DisplayTimePickerDialog(getContext()));
         rootView.findViewById(R.id.thursday_start_time).setOnClickListener(new DisplayTimePickerDialog(getContext()));
@@ -61,7 +73,14 @@ public class WorkTimeFragment extends Fragment {
                 dlg.show();
             }
         });
-        return rootView;
+        return _binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //mViewModel = ViewModelProviders.of(this).get(WorkingTimesViewModel.class);
+        // TODO: Use the ViewModel
     }
 }
 
--- app/src/main/java/com/v8judd/spielhalleplaner/WorkingTimesViewModel.java
+++ app/src/main/java/com/v8judd/spielhalleplaner/WorkingTimesViewModel.java
@@ -1,9 +1,14 @@
 package com.v8judd.spielhalleplaner;
 
 import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
 
 public class WorkingTimesViewModel extends ViewModel {
     // TODO: Implement the ViewModel
 
+    public ObservableField<String> Test = new ObservableField<>("Default Test Text");
 
+    public void t() {
+        Test.notifyChange();
+    }
 }
